d33baeffacb344ccf9c216242c0f1af61d017460
Douglas Dreer <douglasdreer@outlook.com.br>
2026-01-13 09:45:42 -0300

    Commit inicial do projeto

---
+++ src/test/java/io/github/douglasdreer/managerorder/ManagerOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.douglasdreer.managerorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
